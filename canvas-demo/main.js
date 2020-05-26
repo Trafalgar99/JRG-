14f067c5c1a199de370b941b68e042c25d1c912c
Trafalgar99 <1286015267@qq.com>
2020-05-26 21:55:23 +0800

    5.26

--- canvas-demo/main.js
+++ canvas-demo/main.js
@@ -1,71 +1,106 @@
 
 var yyy = document.getElementById('xxx');
 
+autoSetCanvasSize(yyy)
 
-resize()
-
-window.onresize = function () {
-    resize()
-}
-
-var context = yyy.getContext('2d');
+/************************/
+listenToMouse(yyy)
 
 
 
-
-function drawCircle(x, y, radius) {
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2);
-    context.fill()
-}
+function listenToMouse(canvas) {
+    var context = canvas.getContext('2d');
 
 
-var painting = false
-var lastPoint = { x: undefined, y: undefined }
-yyy.onmousedown = function (aaa) {
-    painting = true
-    var x = aaa.clientX
-    var y = aaa.clientY
-    lastPoint = { x: x, y: y }
-
-}
-yyy.onmousemove = function (aaa) {
-    if (painting) {
-        painting = true
+    var using = false
+    var lastPoint = { x: undefined, y: undefined }
+    canvas.onmousedown = function (aaa) {
         var x = aaa.clientX
         var y = aaa.clientY
-        var newPoint = { x: x, y: y }
+        using = true
+        if (eraserEnabled) {
 
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            context.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+
+
+            lastPoint = { x: x, y: y }
+        }
+
     }
-    lastPoint = newPoint
+    canvas.onmousemove = function (aaa) {
+        var x = aaa.clientX
+        var y = aaa.clientY
+
+        if (!using) {
+            return
+        }
+        if (eraserEnabled) {
+
+            context.clearRect(x - 5, y - 5, 10, 10)
+
+        } else {
+
+            var newPoint = {
+                x: x,
+                y: y
+            }
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            lastPoint = newPoint
+        }
+    }
+
+    canvas.onmouseup = function (aaa) {
+        using = false
+
+    }
+    function drawLine(x1, y1, x2, y2) {
+        context.beginPath();
+        context.moveTo(x1, y1)
+        context.lineWidth = 5
+        context.lineTo(x2, y2)
+        context.stroke()
+        context.closePath()
+    }
+    function drawCircle(x, y, radius) {
+        context.beginPath();
+        context.arc(x, y, radius, 0, Math.PI * 2);
+        context.fill()
+    }
 }
-yyy.onmouseup = function (aaa) {
-    painting = false
 
+
+
+
+
+
+var eraserEnabled = false
+eraser.onclick = function () {
+    eraserEnabled = true
+    actions.className = "actions x"
+}
+brush.onclick=function(){
+    eraserEnabled=false
+    actions.className = "actions"
 }
 
 
-function drawLine(x1, y1, x2, y2) {
-    context.beginPath();
-    context.moveTo(x1, y1)
-    context.lineWidth = 5
-    context.lineTo(x2, y2)
-    context.stroke()
-    context.closePath()
+function autoSetCanvasSize(canvas) {
+    setCanvasSize()
+
+    window.onresize = function () {
+        setCanvasSize()
+    }
+    function setCanvasSize() {
+
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+
+        canvas.width = pageWidth
+        canvas.height = pageHeight
+    }
+
 }
-
-
-function resize(){
-  
-    var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  
-  yyy.width = pageWidth
-  yyy.height = pageHeight
-  }
-  
-
 
 
 
