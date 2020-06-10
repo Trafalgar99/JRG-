3eb728d7ff8ff09614d55087729c4502a9422e97
Trafalgar99 <1286015267@qq.com>
2020-06-10 22:33:57 +0800

    6.10

---
+++ resume/main.js
@@ -0,0 +1,36 @@
+// 使加载动画最少展示1s
+setTimeout(function () {
+  siteWelcome.classList.remove("active");
+}, 1000);
+
+// 监听窗口滚动，改变topNavBar的样式
+window.onscroll = function (xxx) {
+  if (window.scrollY > 0) {
+    topNavBar.classList.add("sticky");
+  } else {
+    topNavBar.classList.remove("sticky");
+  }
+};
+let liTags = document.querySelectorAll("nav.menu > ul > li");
+for (let i = 0; i < liTags.length; i++) {
+  liTags[i].onmouseenter = function (x) {
+    x.currentTarget.classList.add("active");
+  };
+
+  liTags[i].onmouseleave = function (x) {
+    x.currentTarget.classList.remove("active");
+  };
+}
+// 锚点跳转
+let aTags = document.querySelectorAll("nav.menu > ul > li > a");
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function (x) {
+    x.preventDefault();
+    let a = x.currentTarget;
+    let href = a.getAttribute("href");
+    let element = document.querySelector(href);
+    let rect = element.getBoundingClientRect();
+    let top = element.offsetTop;
+    window.scrollTo(0, top - 80);
+  };
+}
